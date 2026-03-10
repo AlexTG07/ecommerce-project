d5479c428fcee282984d813d8f9efc1845139182
Alex <timisgabriel@itis-molinari.eu>
2026-03-10 12:32:09 +0100

    Aggiunge docs per GitHub Pages

--- ecommerce-project/docs/user.js
+++ ecommerce-project/docs/user.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000";
+const API_BASE = "https://ecommerce-project-he8a.onrender.com";
 
 const userSelect = document.getElementById("userSelect");
 const userCredits = document.getElementById("userCredits");
